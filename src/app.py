9d0dddc26551bff861432962a552bc179a6440dc
Aruga0918 <81169487+Aruga0918@users.noreply.github.com>
2021-04-22 01:17:25 +0900

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -8,16 +8,17 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, PostbackEvent, TextMessage, TextSendMessage, TemplateSendMessage, TemplateSendMessage, ButtonsTemplate, URIAction
 )
 from youtube import youtubesearch
 from wiki import get_description
 from randomselect import select
+from makequize import Quiz
 app = Flask(__name__)
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-
+quiz = Quiz()
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -44,17 +45,44 @@ def callback():
 #         TextSendMessage(text=event.message.text))
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    query = select(event.message.text)
-    wiki_description = get_description(query)
-    youtube_url = youtubesearch(query)
-    app.logger.debug(wiki_description)
-    app.logger.debug(youtube_url)
-
+    user_id = event.source.user_id
+    app.logger.info(user_id)
+    if quiz.dictio(user_id):
+        if event.message.text == "クイズ":
+            button_messages = quiz.sendfinish()
+            line_bot_api.reply_message(
+                event.reply_token,
+                button_messages
+                )
+    else:
+        query = select(event.message.text)
+        if query != "no heritage":
+            wiki_description = get_description(query)
+            youtube_url = youtubesearch(query)
+            app.logger.debug(query)
+            app.logger.debug(wiki_description)
+            app.logger.debug(youtube_url)
+            quiz.make_quiz(user_id,query)
+            line_bot_api.reply_message(
+                event.reply_token,
+                [TextSendMessage(text=wiki_description),
+                TextSendMessage(text=youtube_url)])
+            return query
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="該当する世界遺産はありません…ごめんなさい"),
+                )
+    #     else:
+    #         line_bot_api.reply_message(
+    #             event.reply_token,
+    #             TextSendMessage(text="クイズと打つとクイズをだすよ！"))
+@handler.add(PostbackEvent)
+def on_postback(event):
+    data = event.postback.data
     line_bot_api.reply_message(
         event.reply_token,
-        [TextSendMessage(text=wiki_description),
-        TextSendMessage(text=youtube_url)])
-
+        TextSendMessage(text=data))
 
 if __name__ == "__main__":
     app.run(debug=True)
